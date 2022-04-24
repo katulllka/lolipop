bd0bee703110dbd1dcfd7b48c6f7d32f3af08377
epalatova <katulllka@bk.ru>
2022-04-24 23:12:02 +0300

    Добавлено удаление сообщений в чате и фиксированное количество сообщений в чате

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ let users = []
 // Создаем переменную для id
 let msgId = 0
 
+// Указываем максимальное количество сообщений в чате
+let maxChatMsg = 10
+
+// Массив с объектами сообщений
+let msgArray = []
+
+//
+let deleteMsgObj
+let editMsgObj
+
+
+
 //Создаем функцию для генерации msgId
 function getMsgId() {
     msgId += 1
-    return msgId
+    return String(msgId)
 }
 
 // Проверяем подключение
@@ -27,15 +39,39 @@ wsServer.on('connection', function (ws) {
     ws.on('message', function (message) {
         // Парсим полученный message json в объект
         let msgObj = JSON.parse(message)
+        let lastMsg
+
+        // Проверка,  если приходит новое сообщение
         if (msgObj.id == null) {
             // Присваиваем новому сообщению id
             msgObj.id = getMsgId()
+            // Удаляем последнее сообщение, если количество сообщений в чате достигло максимума
+            if (msgArray.length === maxChatMsg) {
+                let lastMsgObj = msgArray.shift()
+                lastMsgObj.message = null
+                lastMsg = JSON.stringify(lastMsgObj);
+            }
+            // Добавить в массив сообщений новый месседж
+            msgArray.push(msgObj)
+        // Проверка,  если удаляем существующее сообщение
+        }else if (msgObj.message == null) {
+            deleteMsgObj = msgArray.find(element => element.id === msgObj.id )
+            msgArray.splice(msgArray.indexOf(deleteMsgObj),1)
+        // Проверка,  если редактируем существующее сообщение
+        }else {
+            editMsgObj = msgArray.find(element => element.id === msgObj.id )
+            msgArray[msgArray.indexOf(editMsgObj)].message = msgObj.message
         }
+
+
         message = JSON.stringify(msgObj);
         // Перебираем всех подключенных клиентов
         for (let u of users) {
             // Отправляем им полученное сообщение
             u.connection.send(message)
+            if (lastMsg) {
+                u.connection.send(lastMsg)
+            }
         }
     })
     // Делаем действие при выходе пользователя из чата
